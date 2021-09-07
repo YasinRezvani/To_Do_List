e9daacf07bff9e09393ca14b62505324e2740025
YasinRezvani <yasinrezvani2025@gmail.com>
2021-09-07 09:55:42 +0430

    add document to how work to fo list

--- To_Do_list.py
+++ To_Do_list.py
@@ -1,1 +1,15 @@
 from prettytable import PrettyTable
+
+print("\nTo-Do List Document")
+print("")
+print(
+" 1: Enter add : for add new To-Do\n",
+"2: Enter update : for update To-Do\n",
+"3: Enter delete : for delete To-Do\n",
+"4: Enter view : for view your To-Do\n",
+"5: Enter exit : for exit the program\n"
+)
+
+list_to_do = []
+
+x = PrettyTable()
